a6caf5f46295a8d8fa63f8cb78446ebfbc15ba17
Guilherme Soster <guilhermesosters@gmail.com>
2016-02-27 21:49:17 -0300

    feature: sendMessage
    new feature: send user message

--- basicChat/basicChat.js
+++ basicChat/basicChat.js
@@ -1,23 +1,41 @@
+Messages = new Mongo.Collection("messages");
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    var mesage_data = [{
+        message: "ola!"
+    }, {
+        message: "first post!"
+    }];
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.messages.helpers({
+        messages: function () {
+            return Messages.find();
+        }
+    });
+
+
+
+    Template.formSendMessage.events({
+        "submit form": function (event, template) {
+            event.preventDefault();
+            alert("ok!");
+            var message = template.find('.sendMessage').value;
+            Messages.insert({
+                message: message,
+                createdAt: new Date()
+            });
+        }
+    });
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        if (Messages.find().count() === 0) {
+            Messages.insert({
+                message: "ola",
+                createdAt: new Date()
+            });
+        }
+    });
 }
