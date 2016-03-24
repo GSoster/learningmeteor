498cd39d75432b54119226e6fff2d57242754ddc
GSoster <guilhermesosters@gmail.com>
2016-03-24 00:17:27 -0300

    added date helper; made it all work!

--- textcircle/textcircle.js
+++ textcircle/textcircle.js
@@ -1,15 +1,28 @@
-this.Documents = new Mongo.collection("documents");//this so the package sharejs can use it.
+this.Documents = new Mongo.Collection("documents");//this so the package sharejs can use it.
 
 if (Meteor.isClient) {
+    Template.editor.helpers({
+        docid: function () {
+            var doc = Documents.findOne();
+            if (doc) {
+                return doc._id;
+            }
+            return undefined;
+        }
+    });
 
-
+    Template.date_display.helpers({
+        current_date : function(){
+            return new Date();
+        }
+    });
 
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
         if (!Documents.findOne()) {
-            Documents.insert({title: 'my new content'});
+            Documents.insert({title: "my new content"});
         }
     });
 }
