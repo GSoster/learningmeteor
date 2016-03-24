1cbf30f39b02efbc554e01b0b13a2c9495d21736
GSoster <guilhermesosters@gmail.com>
2016-03-24 00:21:46 -0300

    making current time work based on reactive sessions

--- textcircle/textcircle.js
+++ textcircle/textcircle.js
@@ -1,6 +1,16 @@
 this.Documents = new Mongo.Collection("documents");//this so the package sharejs can use it.
 
+
+
 if (Meteor.isClient) {
+    
+    
+    //Update the current time in the session
+    //every 1 second
+    Meteor.setInterval(function(){
+        Session.set('current_date', new Date());
+    }, 1000);
+    
     Template.editor.helpers({
         docid: function () {
             var doc = Documents.findOne();
@@ -13,7 +23,7 @@ if (Meteor.isClient) {
 
     Template.date_display.helpers({
         current_date : function(){
-            return new Date();
+            return Session.get('current_date');
         }
     });
 
