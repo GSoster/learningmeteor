f0fad7e520ff0ff692b2dcc895c7ffe1e7b07ee3
Guilherme Soster <guilhermesosters@gmail.com>
2016-11-08 15:59:35 -0200

    now input (add task) form will only appear to logged in users

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -1,5 +1,5 @@
 import {Template} from 'meteor/templating';
 import {Tasks} from '../imports/api/tasks.js';
 
-import '../imports/startup/accounts-config.js';
+import '../imports/startup/accounts-config.js'
 import '../imports/ui/body.js';
