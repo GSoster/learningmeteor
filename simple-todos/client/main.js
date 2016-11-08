60b7710734119cec8b5d7f6fafcb0697ae21c27d
Guilherme Soster <guilhermesosters@gmail.com>
2016-11-08 14:00:28 -0200

    now tasks that are displayed have origin in the database

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -1,1 +1,10 @@
+import {Template} from 'meteor/templating';
+import {Tasks} from '../imports/api/tasks.js';
+
 import '../imports/ui/body.js';
+
+Template.body.helpers({
+  tasks(){
+    return Tasks.find({});
+  },
+});
--- simple-todos/server/main.js
+++ simple-todos/server/main.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/tasks.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  //db.tasks.insert({ text: "Hello world!", createdAt: new Date() });
 });
